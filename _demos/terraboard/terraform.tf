9ad94ea95ba1d264f05fb02cb4b0d3e2b7d9bdc2
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-02-03 15:54:05 +0100

    Remove old stuff

--- _demos/terraboard/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraboard-demo"
-    key            = "env:/terraform_state"
-    profile        = "sandbox"
-    region         = "eu-west-1"
-    dynamodb_table = "demo-terraform-statelock"
-  }
-}
