797c10f357c12747b25f1b2664622935aa4eb2b0
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-02-03 16:14:24 +0100

    Modify remote key

--- _demos/terraboard/wordpress/terraform.tf
+++ _demos/terraboard/wordpress/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraboard-demo"
-    key            = "wordpress"
+    key            = "wordpress_state"
     profile        = "sandbox"
     region         = "eu-west-1"
     dynamodb_table = "demo-terraform-statelock"
